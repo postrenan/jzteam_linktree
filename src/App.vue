618e796b5da3f688e4f116f007645f564bcec5d9
Bick Renan <renanbickdev@gmail.com>
2024-06-26 11:59:10 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <RouterView />
+  <SpeedInsights />
 </template>
 
 <script setup>
 import {RouterView } from 'vue-router'
+import { SpeedInsights } from '@vercel/speed-insights/vue';
 </script>
 
 <style scoped>
